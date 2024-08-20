1525f198972cdd6c58b4fbd897df8b825e22111b
Patricio Ascencio <patricio.ascencio@karibu.cl>
2024-08-20 01:12:39 -0400

    fix: Readme

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name="logging-commons",
     version="0.1.10",
     url="https://github.com/KaribuLab/python-logging-commons",
-    long_description_content_type = "text/markdown",
-    long_description=open("README.md",encoding="UTF-8").read(),
     license="Apache Software License",
     author="Patricio Ascencio",
     author_email="paascencio@gmail.com",
